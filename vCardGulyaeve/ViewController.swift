ad35234730957b226862caa981a6537cb2f39a96
gulyaeve <gulyaev.e@icloud.com>
2021-06-02 12:43:09 +0300

    Initial Commit

---
+++ vCardGulyaeve/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vCardGulyaeve
+//
+//  Created by Евгений Гуляев on 02.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
